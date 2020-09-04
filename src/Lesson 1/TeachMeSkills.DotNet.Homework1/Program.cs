148fd67a1baf676f08acb2a7bdc5a04de68645f2
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-09-04 21:49:06 +0300

    feat: solution of homework 1

---
+++ src/Lesson 1/TeachMeSkills.DotNet.Homework1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your date: ");
+            string userInput = Console.ReadLine();
+            bool result = DateTime.TryParse(userInput, out DateTime date);
+            if (result)
+            {
+                Console.WriteLine(date.DayOfWeek);
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

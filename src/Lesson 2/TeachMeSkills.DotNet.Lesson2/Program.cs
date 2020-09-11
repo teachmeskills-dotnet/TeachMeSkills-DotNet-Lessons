0030fb682049bd05b3859c2e2ecf78d3971ec964
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-09-11 21:45:21 +0300

    feat: examples of lesson 2. part 2

--- src/Lesson 2/TeachMeSkills.DotNet.Lesson2/Program.cs
+++ src/Lesson 2/TeachMeSkills.DotNet.Lesson2/Program.cs
@@ -4,6 +4,9 @@ namespace TeachMeSkills.DotNet.Lesson2
 {
     class Program
     {
+        private static int _classScopeValue = 10;
+        private int MyProperty { get; set; }
+
         static void Main(string[] args)
         {
             var i = 0; // постфиксный
@@ -87,6 +90,9 @@ namespace TeachMeSkills.DotNet.Lesson2
             Console.WriteLine(str1234);
             string str321 = nameof(str321);
             Console.WriteLine(str321);
+            string anotherUserName = nameof(Main);
+            anotherUserName = "test value";
+            Console.WriteLine($"nameof: {anotherUserName}");
 
             var array1 = new int[5];
             int[] array2 = new int[] { 1, 3, 5, 7, 9 };
@@ -115,11 +121,57 @@ namespace TeachMeSkills.DotNet.Lesson2
                 Console.WriteLine(strItem);
             }
 
-
-
-
+            ShowMessage();
+            UseParams();
 
             Console.ReadLine();
+        }
+
+        private static void UseParams()
+        {
+            var a = nameof(SumArray);
+            var b = 2;
+            var c = 3;
+            var d = 4;
+
+            var arr1 = new int[] { 1, 2, 3 };
+            int[] arr2 = { 1, 2, 3 };
+
+            SumArray(a, new int[] { 1, 2, 3 });
+            SumArray(a, arr1);
+            SumArray(a, arr2);
+            SumArray(a, b, c, d);
+        }
+        private static void SumArray(string a, params int[] array)
+        {
+            _classScopeValue = 123;
+            int scopeValue = 1;
+            Console.WriteLine($"{a} Method");
+            Console.WriteLine($"{nameof(array.Length)} " + array.Length);
+            foreach (var item in array)
+            {
+                _classScopeValue = 123;
+                var anotherScopeValue = 2;
+                Console.WriteLine(item);
+            }
+        }
+
+        static void ShowMessage()
+        {
+            //int x = GetValue(y: "123", x: 90);
+            int val1 = 90;
+            string val2 = "123";
+            (bool operationResult, int sum) = GetValue(val1, val2);
+            Console.WriteLine(operationResult + " " + sum);
+            Console.WriteLine(val1);
+        }
+
+        static (bool operationResult, int sum) GetValue(int x, string y, int z = 10)
+        {
+            x++;
+            Console.WriteLine($"y = {y}");
+            var p = x + z; // 101
+            return (p > 100, p);
         }
     }
 }

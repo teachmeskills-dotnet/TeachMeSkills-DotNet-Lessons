3991debf20ca4589b4e5234df466dbafa4d9fe8c
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-10-14 21:01:20 +0300

    feat: examples of lesson 5 (delegate). part 1

---
+++ src/Lesson 5/TeachMeSkills.DotNet.Lesson5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Lesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var simple = new SimpleDelegate();
+            simple.Run();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/Lesson 5/TeachMeSkills.DotNet.Lesson5/SimpleDelegate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Lesson5
+{
+    delegate void Message();
+
+    public class SimpleDelegate
+    {
+        public void Run()
+        {
+            Message message;
+            if (DateTime.Now.Hour < 12)
+            {
+                message = GoodMorning;
+            }
+            else
+            {
+                message = GoodEvening;
+                message += GoodMorning;
+                message += GoodEvening;
+            }
+            message();
+            message -= GoodEvening;
+            message = GoodMorning;
+            Console.WriteLine();
+            message();
+        }
+
+        private void GoodMorning()
+        {
+            Console.WriteLine("Good morning!");
+        }
+
+        private void GoodEvening()
+        {
+            Console.WriteLine("Good evening!");
+        }
+    }
+}

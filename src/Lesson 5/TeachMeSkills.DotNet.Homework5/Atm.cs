10a00b233ac72b1a1058be997c904ae1dba87057
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-10-16 23:59:19 +0300

    feat: solution of homework 5

---
+++ src/Lesson 5/TeachMeSkills.DotNet.Homework5/Atm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TeachMeSkills.DotNet.Homework5
+{
+    public class Atm
+    {
+        public event Action<decimal, string> BalanceHandler;
+        private decimal _balance = 0M;
+
+        public void GetMoney(decimal money)
+        {
+            _balance -= money;
+            BalanceHandler?.Invoke(_balance, "get");
+        }
+
+        public void PutMoney(decimal money)
+        {
+            _balance += money;
+            BalanceHandler?.Invoke(_balance, "put");
+        }
+
+        public void ShowInfo()
+        {
+            var eventName = "show";
+            BalanceHandler?.Invoke(_balance, eventName);
+
+            // Вызов конкретного метода из всего списка подписок
+            //var listOfMethods = BalanceHandler?.GetInvocationList();
+            //var firstDelegate = listOfMethods[0];
+            //var methodName = firstDelegate.Method.Name;
+            //List<object> args = new List<object>
+            //{
+            //    _balance,
+            //    eventName
+            //};
+            //var progClass = new Program();
+            //var result = progClass.GetType().GetMethod(methodName).Invoke(this, args.ToArray());
+        }
+    }
+}
---
+++ src/Lesson 5/TeachMeSkills.DotNet.Homework5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Homework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Atm atm = new Atm();
+            atm.BalanceHandler += GetInfo;
+            atm.BalanceHandler += GetInfo;
+            atm.BalanceHandler += GetInfo;
+            atm.BalanceHandler += GetInfo;
+
+            
+
+            //atm.BalanceHandler += (amount, operation) => Console.WriteLine($"Operation {operation}. Current balance: {amount}");
+            atm.PutMoney(10M);
+            atm.GetMoney(5M);
+            atm.ShowInfo();
+
+            Console.ReadKey();
+        }
+
+        public static void GetInfo(decimal amount, string operation)
+        {
+            Console.WriteLine($"Operation {operation}. Current balance: {amount}");
+        }
+    }
+}

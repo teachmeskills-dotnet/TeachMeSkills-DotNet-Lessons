17f6f3ca1e7243ebdb2f34682e9d6cc8738878cd
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-10-21 21:18:44 +0300

    feat: examples of lesson 6. part 1

---
+++ src/Lesson 6/TeachMeSkills.DotNet.Lesson6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var TMSCollection = new TmsCollection();
+            TMSCollection.Run();
+        }
+    }
+}
---
+++ src/Lesson 6/TeachMeSkills.DotNet.Lesson6/TmsCollection.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TeachMeSkills.DotNet.Lesson6
+{
+    public class SimpleClass
+    {
+        public int Number { get; set; }
+        public string String { get; set; }
+    }
+    public class TmsCollection
+    {
+        public void Run()
+        {
+            List<int> list = new List<int>
+            {
+                1,
+                2,
+                3,
+            };
+
+            list.Clear();
+            var elem = list.Find(x => x == 2);
+
+            List<SimpleClass> simpleList = new List<SimpleClass>
+            {
+                // добавили элемент в коллекцию - пример 1
+                new SimpleClass
+                {
+                    Number = 1,
+                    String = "str"
+                }
+            };
+
+            // добавили элемент в коллекцию - пример 2
+            simpleList.Add(new SimpleClass
+            {
+                Number = 2,
+                String = "str2",
+            });
+            foreach (var item in simpleList)
+            {
+                Console.WriteLine(item);
+            }
+
+            var findedElement = simpleList.Find(x => x.String == "str2");
+
+            Predicate<SimpleClass> match = x => x.String == "str2";
+            Console.WriteLine(match);
+            var findedElement1 = simpleList.Find(match);
+
+            Predicate<SimpleClass> predicateMatch =
+                delegate (SimpleClass bulka) { return bulka.String == "str2"; };
+            var findedElement2 = simpleList.Find(predicateMatch);
+
+            var findedElement3 = simpleList.Find(
+                delegate (SimpleClass bulka) { return bulka.String == "str2"; });
+
+            IEnumerable<int> eList = new List<int>();
+            //eList.Add(1);
+
+            var listByMethod = GetRandomList();
+
+            //foreach (var item in GetRandomList())
+            //{
+
+            //}
+
+            Console.WriteLine();
+        }
+
+        private IEnumerable<int> GetRandomList()
+        {
+            var rand = new Random();
+            var list = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(rand.Next(-10, 10));
+            }
+
+            return list1;
+        }
+    }
+}

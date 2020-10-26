04526430827f2be7365cc790e8762f5dbc6afcc7
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-10-26 21:06:40 +0300

    feat: examples of regex

--- src/Lesson 6/TeachMeSkills.DotNet.Lesson6/Program.cs
+++ src/Lesson 6/TeachMeSkills.DotNet.Lesson6/Program.cs
@@ -8,12 +8,15 @@ namespace TeachMeSkills.DotNet.Lesson6
         {
             //var TMSCollection = new TmsCollection();
             //TMSCollection.Run();
-            
+
             //var linqSamples = new Linq();
             //linqSamples.Run();
 
-            var newlinqSapmles = new LinqSamples();
-            newlinqSapmles.Run();
+            //var newlinqSapmles = new LinqSamples();
+            //newlinqSapmles.Run();
+
+            var Regex = new RegexSample();
+            Regex.Run();
 
             Console.ReadKey();
         }

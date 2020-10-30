cb0a7db4afdfc106b301495a6ff2a4d91a61c65e
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-10-30 21:51:03 +0300

    feat: examples of lesson 6. part 5

--- src/Lesson 6/TeachMeSkills.DotNet.Lesson6/LinqSamples.cs
+++ src/Lesson 6/TeachMeSkills.DotNet.Lesson6/LinqSamples.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace TeachMeSkills.DotNet.Lesson6
 {
@@ -93,8 +95,8 @@ namespace TeachMeSkills.DotNet.Lesson6
             var resultToDictionaryWithCond = english2German
                 .ToDictionary(
                     k => k.EnglishSalute,
-                    v => v.GermanSalute.Count() > 9 
-                        ? "Odd" 
+                    v => v.GermanSalute.Count() > 9
+                        ? "Odd"
                         : "Even");
 
             var toLookup = defaultArray.ToLookup(n => n > 10);
@@ -129,7 +131,164 @@ namespace TeachMeSkills.DotNet.Lesson6
 
             var enumerableRange = Enumerable.Range(0, 10);
             var enumerableRepeat = Enumerable.Repeat("Hello", 5);
+
+
+            var english2germanList = new List<English2German>
+            {
+                new English2German
+                {
+                    EnglishSalute = "Hello",
+                    GermanSalute = "qwe"
+                },
+                new English2German
+                {
+                    EnglishSalute = "Hi",
+                    GermanSalute = "asd"
+                },
+                new English2German
+                {
+                    EnglishSalute = "Hello",
+                    GermanSalute = "zxc"
+                }
+            };
+
+            var grouped = english2germanList.GroupBy(x => x.EnglishSalute);
+            var groupedWithCond = english2germanList.GroupBy(x => x.EnglishSalute == "Hi");
+
+            foreach (var item in groupedWithCond)
+            {
+                Console.WriteLine(item.Key);
+                foreach (var anotherItem in item)
+                {
+                    Console.WriteLine($"Value: {anotherItem.EnglishSalute} {anotherItem.GermanSalute}");
+                }
+            }
+
+            var dogCollection = new List<Dog>
+            {
+                new Dog
+                {
+                    Age = 1,
+                    Name = "Alice"
+                },
+                new Dog
+                {
+                    Age = 2,
+                    Name = "Dog2"
+                }
+            };
+
+            var catCollection = new List<Cat>
+            {
+                new Cat
+                {
+                    Weight = 101,
+                    Name = "Alice"
+                },
+                new Cat
+                {
+                    Weight = 102,
+                    Name = "Barsik"
+                }
+            };
+
+            var totalCollection = dogCollection
+                .Select(dog => new
+                {
+                    dog.Name,
+                    Type = dog.GetType().Name,
+                    Table = new
+                    {
+                        Data = dog.Age,
+                        Type = nameof(dog.Age)
+                    },
+                })
+                .Concat(catCollection
+                    .Select(cat => new
+                    {
+                        cat.Name,
+                        Type = cat.GetType().Name,
+                        Table = new
+                        {
+                            Data = cat.Weight,
+                            Type = nameof(cat.Weight)
+                        },
+                    }))
+                .OrderBy(x => x.Name)
+                .ThenBy(x => x.Type.Length)
+                .ThenByDescending(x => x.Table.Type);
+                //.GroupBy(x => x.Name);
+
+            var sortedCollection = defaultArray.OrderBy(x => x);
+            var sortedByDescCollection = defaultArray.OrderByDescending(x => x);
+
+            string[] names = { "Name1", "Name2", "Name3" };
+            DateTime[] dates = { new DateTime(2000, 10, 30), new DateTime(2001, 1, 1), new DateTime(2002, 1, 1) };
+
+            Func<string, DateTime, string> FuncDelegate = 
+                (name, date) => $"Name: {name}, Date: {date}";
+            var zipped = names.Zip(dates, FuncDelegate);
+            var zippedTuple = names.Zip(dates);
+
+            var skipCollection = defaultArray.Skip(2);
+
+            var copyDefaultArray = defaultArray.Take(1)
+                .Concat(defaultArray.Skip(2))
+                .ToArray();
+
+            int[] customArray = { 3, 3, 3, 4, 7, 8, 9 };
+            var qqq = customArray.SkipWhileWithTakeCustom(x => x == 3, 4); // 4,7
+
+            var newCustomArrayWithPlusValue = customArray
+                .Select(i => i + 123);
+
+            var newCustomCollectionAll = customArray.All(x => x == 3);
+            var newCustomCollectionAny = customArray.Any(x => x == 3);
+            var newCustomCollectionContains = customArray.Contains(3);
+
+            var newCustomCollectionWhere = customArray.Where(x => x == 3 || x == 7);
+
         }
+    }
+
+    static class LinqCustom
+    {
+        public static int[] SkipWhileWithTakeCustom(this int[] value, Predicate<int> predicate, int count)
+        {
+            var state = true;
+            var newCollection = new List<int>();
+            foreach (var item in value)
+            {
+                if (predicate(item) && count != 0 && state) // x == 3
+                {
+                    continue;
+                }
+
+                if (count != 0)
+                {
+                    state = false;
+                    count--;
+                    newCollection.Add(item);
+                    continue;
+                }
+            }
+
+            return newCollection.ToArray();
+        }
+
+    }
+    
+
+    class Dog
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+
+    class Cat
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
     }
 
     class English2German
